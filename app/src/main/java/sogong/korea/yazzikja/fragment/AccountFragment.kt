3b3757eed5c9b80b977902b8fab2534cd859dca5
cheong <hwa1130k@gmail.com>
2018-12-07 20:56:44 +0900

    account fragment layout has been set

--- app/src/main/java/sogong/korea/yazzikja/fragment/AccountFragment.kt
+++ app/src/main/java/sogong/korea/yazzikja/fragment/AccountFragment.kt
@@ -5,19 +5,14 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import com.bumptech.glide.Glide
-import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import kotlinx.android.synthetic.main.fragment_account.*
 import sogong.korea.yazzikja.R
 import sogong.korea.yazzikja.model.UserModel
 
@@ -30,8 +25,8 @@ class AccountFragment : Fragment() {
     private var editTextNickname: EditText ? = null
     private var editTextLocation: EditText ? = null
     private var editTextIntroduction: EditText ? = null
-    private var imageButtonPhoto: ImageButton? = null
-    private var imageButtonConfirm: ImageButton? = null
+    private var imageButtonPhotoSelect: ImageButton? = null
+    private var imageButtonConfirm: Button? = null
 
     private var imageViewUserPhotoLarge: ImageView? = null
     private var imageViewUserPhoto: ImageView? = null
@@ -46,14 +41,14 @@ class AccountFragment : Fragment() {
         textViewNickname = view.findViewById<View>(R.id.accountfragment_textview_nickname) as TextView
         textViewLocation = view.findViewById<View>(R.id.accountfragment_textview_location) as TextView
         textViewNumLike = view.findViewById<View>(R.id.accountfragment_textview_num_like) as TextView
-        editTextNickname = accountfragment_edittext_nickname
-        editTextLocation = accountfragment_edittext_introduction
-        editTextIntroduction = accountfragment_edittext_introduction
-        imageButtonPhoto = accountfragment_button_confirm
-        imageButtonConfirm = accountfragment_button_confirm
+        editTextNickname = view.findViewById(R.id.accountfragment_edittext_nickname) as EditText
+        editTextLocation = view.findViewById(R.id.accountfragment_edittext_introduction) as EditText
+        editTextIntroduction = view.findViewById(R.id.accountfragment_edittext_introduction) as EditText
+        imageButtonPhotoSelect = view.findViewById<View>(R.id.accountfragment_photo_select) as ImageButton
+        imageButtonConfirm = view.findViewById<View>(R.id.accountfragment_button_confirm) as Button
 
         imageViewUserPhotoLarge = view.findViewById<View>(R.id.accountfragment_profile_background_image_large) as ImageView
-        imageViewUserPhoto = accountfragment_user_photodisplay
+        imageViewUserPhoto = view.findViewById<View> (R.id.accountfragment_user_photodisplay) as ImageView
 
 //        val myUid = FirebaseAuth.getInstance().currentUser!!.uid
         var userModel: UserModel ? = null
@@ -61,19 +56,22 @@ class AccountFragment : Fragment() {
         FirebaseDatabase.getInstance().reference.child("users/$uid").addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 userModel = dataSnapshot.getValue(UserModel::class.java)
-                textViewNickname!!.text = userModel?.userNickname
-                textViewLocation!!.text = userModel?.userLocation
-                textViewNumLike!!.text = userModel?.userNumLike.toString()
-                editTextNickname!!.setText(userModel?.userNickname)
-                editTextLocation!!.setText(userModel?.userLocation)
-                editTextIntroduction!!.setText(userModel?.userIntroduction)
+                textViewNickname?.text = userModel?.userNickname
+                textViewLocation?.text = userModel?.userLocation
+                if(userModel?.userNumLike.toString().isNullOrBlank()) textViewNumLike?.text = "0"
+                else textViewNumLike?.text = userModel?.userNumLike.toString()
+
+                editTextNickname?.setText(userModel?.userNickname)
+                editTextLocation?.setText(userModel?.userLocation)
+                editTextIntroduction?.setText(userModel?.userIntroduction)
             /*    GlideApp.with(inflater.context)
                     .load(userModel?.profileImageUri)
                     .placeholder(R.drawable.profile_background)
                     .fitCenter()
                     .into(imageViewUserPhoto);*/
 
-                Glide.with(inflater.context).load(userModel?.profileImageUri).apply(RequestOptions().circleCrop()).into(imageViewUserPhoto!!)
+                Glide.with(inflater?.context).load(userModel?.profileImageUri).apply(RequestOptions().circleCrop()).into(imageViewUserPhoto!!)
+                Glide.with(inflater?.context).load(userModel?.profileImageUri).apply(RequestOptions()).into(imageViewUserPhotoLarge!!)
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
@@ -81,12 +79,9 @@ class AccountFragment : Fragment() {
             }
         })
 
+ //       imageButtonConfirm!!.setOnClickListener{
 
-
-
-        imageButtonConfirm!!.setOnClickListener{
-
-        }
+   //     }
 
         return view
     }
